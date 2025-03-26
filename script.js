6703d32897b3864ed0d2559758b978f791c5876a
santiagodelpercioR <santiagodelpercio@gmail.com>
2025-03-26 18:20:50 -0300

    ordeno la estructura css

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const apiKey = "9e7d89b7";
-const movieName = document.getElementById("movieName");
-const searchbutton = document.getElementById("searchbutton");
+let searchForm = document.getElementById("searchForm");
 const posterElement = document.getElementById("poster");
 const titleElement = document.getElementById("title");
 const directorElement = document.getElementById("director");
@@ -13,9 +12,10 @@ const countryElement = document.getElementById("country");
 const languageElement = document.getElementById("language");
 const metascoreElement = document.getElementById("metascore");
 
-searchbutton.addEventListener("click", function(){
-    let movieTitle = movieName.value;
-    fetch(`https://www.omdbapi.com/?t=${movieTitle}&apikey=${apiKey}`)
+searchForm.addEventListener("submit", function(event){
+    event.preventDefault();
+    let movieName = document.getElementById("movieName");
+    fetch(`https://www.omdbapi.com/?t=${movieName.value}&apikey=${apiKey}`)
         .then(response => response.json()) // Convertimos la respuesta a JSON
         .then(data => {
             console.log(data);
@@ -33,33 +33,53 @@ searchbutton.addEventListener("click", function(){
                 posterElement.src = data.Poster;
             }
             else{
-                posterElement.src = "";
+                vaciarDatos({title:"Pelicula no encontrada"});
             }
         })
         .catch(error => console.error("Hubo un error:", error));
 })
 
 
-   /* let movieTitle = movieName.value;
-    fetch(`https://www.omdbapi.com/?t=inception&apikey=${apiKey}`)
-        .then(response => response.json()) // Convertimos la respuesta a JSON
-        .then(data => {
-            console.log(data);
-            if(data.Response === "True"){
-                titleElement.textContent = `Título: ${data.Title}`;
-                yearElement.textContent = `Año: ${data.Year}`;
-                directorElement.textContent = `Director: ${data.Director}`;
-                actorsElement.textContent = `Actores: ${data.Actors}`;
-                plotElement.textContent = `Descripción: ${data.Plot}`;
-                genreElement.textContent = `Género: ${data.Genre}`;
-                runtimeElement.textContent = `Duración: ${data.Runtime}`;
-                countryElement.textContent = `País: ${data.Country}`;
-                languageElement.textContent = `Idioma: ${data.Language}`;
-                metascoreElement.textContent = `Metascore: ${data.Metascore}`;
-                posterElement.src = data.Poster;
-            }
-            else{
-                posterElement.src = "";
-            }
-        })
-        .catch(error => console.error("Hubo un error:", error));*/
+    // let movieName = document.getElementById("movieName");
+    // fetch(`https://www.omdbapi.com/?t=speed racer&apikey=${apiKey}`)
+    //     .then(response => response.json()) // Convertimos la respuesta a JSON
+    //     .then(data => {
+    //         console.log(data);
+    //         if(data.Response === "True"){
+    //             titleElement.textContent = `Título: ${data.Title}`;
+    //             yearElement.textContent = `Año: ${data.Year}`;
+    //             directorElement.textContent = `Director: ${data.Director}`;
+    //             actorsElement.textContent = `Actores: ${data.Actors}`;
+    //             plotElement.textContent = `Descripción: ${data.Plot}`;
+    //             genreElement.textContent = `Género: ${data.Genre}`;
+    //             runtimeElement.textContent = `Duración: ${data.Runtime}`;
+    //             countryElement.textContent = `País: ${data.Country}`;
+    //             languageElement.textContent = `Idioma: ${data.Language}`;
+    //             metascoreElement.textContent = `Metascore: ${data.Metascore}`;
+    //             posterElement.src = data.Poster;
+    //         }
+    //         else{
+    //             vaciarDatos({title:"Pelicula no encontrada"});
+    //         }
+    //     })
+    //     .catch(error => console.error("Hubo un error:", error));
+
+
+
+function vaciarDatos({ 
+    title = "", year = "", director = "", actors = "", 
+    plot = "", genre = "", runtime = "", country = "", 
+    language = "", metascore = "", poster = "" 
+}) {
+    titleElement.textContent = title;
+    yearElement.textContent = year;
+    directorElement.textContent = director;
+    actorsElement.textContent = actors;
+    plotElement.textContent = plot;
+    genreElement.textContent = genre;
+    runtimeElement.textContent = runtime;
+    countryElement.textContent = country;
+    languageElement.textContent = language;
+    metascoreElement.textContent = metascore;
+    posterElement.src = poster;
+}
