cf68355980a073dfa11becbc48dc7058f033081f
santiagodelpercioR <santiagodelpercio@gmail.com>
2025-04-02 11:12:23 -0300

    agrego fondo negro y saco bordes

--- script.js
+++ script.js
@@ -40,29 +40,29 @@ searchForm.addEventListener("submit", function(event){
 })
 
 
-    // let movieName = document.getElementById("movieName");
-    // fetch(`https://www.omdbapi.com/?t=speed racer&apikey=${apiKey}`)
-    //     .then(response => response.json()) // Convertimos la respuesta a JSON
-    //     .then(data => {
-    //         console.log(data);
-    //         if(data.Response === "True"){
-    //             titleElement.textContent = `Título: ${data.Title}`;
-    //             yearElement.textContent = `Año: ${data.Year}`;
-    //             directorElement.textContent = `Director: ${data.Director}`;
-    //             actorsElement.textContent = `Actores: ${data.Actors}`;
-    //             plotElement.textContent = `Descripción: ${data.Plot}`;
-    //             genreElement.textContent = `Género: ${data.Genre}`;
-    //             runtimeElement.textContent = `Duración: ${data.Runtime}`;
-    //             countryElement.textContent = `País: ${data.Country}`;
-    //             languageElement.textContent = `Idioma: ${data.Language}`;
-    //             metascoreElement.textContent = `Metascore: ${data.Metascore}`;
-    //             posterElement.src = data.Poster;
-    //         }
-    //         else{
-    //             vaciarDatos({title:"Pelicula no encontrada"});
-    //         }
-    //     })
-    //     .catch(error => console.error("Hubo un error:", error));
+ /*   let movieName = document.getElementById("movieName");
+    fetch(`https://www.omdbapi.com/?t=speed racer&apikey=${apiKey}`)
+        .then(response => response.json()) // Convertimos la respuesta a JSON
+        .then(data => {
+            console.log(data);
+            if(data.Response === "True"){
+                titleElement.textContent = `Título: ${data.Title}`;
+                yearElement.textContent = `Año: ${data.Year}`;
+                directorElement.textContent = `Director: ${data.Director}`;
+                actorsElement.textContent = `Actores: ${data.Actors}`;
+                plotElement.textContent = `Descripción: ${data.Plot}`;
+                genreElement.textContent = `Género: ${data.Genre}`;
+                runtimeElement.textContent = `Duración: ${data.Runtime}`;
+                countryElement.textContent = `País: ${data.Country}`;
+                languageElement.textContent = `Idioma: ${data.Language}`;
+                metascoreElement.textContent = `Metascore: ${data.Metascore}`;
+                posterElement.src = data.Poster;
+            }
+            else{
+                vaciarDatos({title:"Pelicula no encontrada"});
+            }
+        })
+        .catch(error => console.error("Hubo un error:", error));*/
 
 
 
